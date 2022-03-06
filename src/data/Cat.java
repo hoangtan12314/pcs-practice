b327faa3361967b3d62955d019ad8f09cb76f9d3
hoangtan12314 <hoangtan12314@gmail.com>
2022-03-06 15:59:42 +0700

    Implementing Pet, Cat, Dog in Data

--- src/data/Cat.java
+++ src/data/Cat.java
@@ -1,4 +1,22 @@
 package data;
 
-public class Cat {
+import java.util.Random;
+
+public class Cat extends Pet{
+    private static final double MAX_SPD = 15.0;
+
+    public Cat(String id, String name, int yob, double weight) {
+        super(id, name, yob, weight);
+    }
+
+    @Override
+    public double run() {
+        return new Random().nextDouble() * MAX_SPD;
+    }
+
+    @Override
+    public void showProfile() {
+        System.out.printf("|%6s|%-10s|%4d|%4.1f|%4.1f|\n",
+                id, name, yob, weight, run());
+    }
 }
--- src/data/Pet.java
+++ src/data/Pet.java
@@ -1,4 +1,59 @@
 package data;
 
-public class Pet {
+import java.util.Comparator;
+
+public abstract class Pet implements Comparable<Pet> {
+    protected String id;
+    protected String name;
+    protected int yob;
+    protected double weight;
+
+    public Pet(String id, String name, int yob, double weight) {
+        this.id = id;
+        this.name = name;
+        this.yob = yob;
+        this.weight = weight;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYob() {
+        return yob;
+    }
+
+    public void setYob(int yob) {
+        this.yob = yob;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public abstract double run();
+
+    public abstract void showProfile();
+
+    @Override
+    // Comparing pets according to their id
+    public int compareTo(Pet that) {
+        return this.getId().compareToIgnoreCase(that.getId());
+    }
 }
