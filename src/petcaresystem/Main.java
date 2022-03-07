5687ec2466f2681c3daecea75b504d96d0f8369b
hoangtan12314 <hoangtan12314@gmail.com>
2022-03-07 18:32:56 +0700

    Implementing Main. Finish testing all funciton and menu.

--- src/petcaresystem/Main.java
+++ src/petcaresystem/Main.java
@@ -1,4 +1,59 @@
 package petcaresystem;
 
+import data.Cabinet;
+import ui.Menu;
+
 public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu("Pet Care System");
+        menu.addOption("1. Add a cat profile");
+        menu.addOption("2. Add a dog profile");
+        menu.addOption("3. Search a pet");
+        menu.addOption("4. Update a pet");
+        menu.addOption("5. Remove a pet");
+        menu.addOption("6. Print pet profile list ascending by ID");
+        menu.addOption("7. Print pet profile list ascending by Name");
+        menu.addOption("8. Quit");
+
+        Cabinet cabinet = new Cabinet();
+        int choice;
+
+        do {
+            menu.printMenu();
+            choice = menu.retrieveChoice();
+
+            switch (choice){
+                case 1:
+                    System.out.println("You are preparing to add a cat profile: ");
+                    cabinet.addNewCat();
+                    break;
+                case 2:
+                    System.out.println("You are preparing to add a dog profile: ");
+                    cabinet.addNewDog();
+                    break;
+                case 3:
+                    System.out.println("You are preparing to search for a pet");
+                    cabinet.searchPetById();
+                    break;
+                case 4:
+                    System.out.println("You are preparing to update information for a pet");
+                    cabinet.updatePetInfo();
+                    break;
+                case 5:
+                    System.out.println("You are preparing remove a pet");
+                    cabinet.removePet();
+                    break;
+                case 6:
+                    cabinet.printPetListAscendingByID();
+                    break;
+                case 7:
+                    cabinet.printPetListAscendingByName();
+                    break;
+                case 8:
+                    System.out.println("Exiting System. See you again.");
+                    break;
+            }
+
+        } while (choice != 8);
+    }
 }
--- src/test/Test.java
+++ src/test/Test.java
@@ -4,13 +4,13 @@ import data.Cabinet;
 import util.Handelnput;
 
 public class Test {
-    public static void main(String[] args) {
-        Cabinet cabinet = new Cabinet();
-        cabinet.addNewCat();
-        cabinet.addNewDog();
-        cabinet.addNewCat();
-        cabinet.addNewDog();
-        cabinet.printPetListAscendingByID();
-        cabinet.printPetListAscendingByName();
-    }
+//    public static void main(String[] args) {
+//        Cabinet cabinet = new Cabinet();
+//        cabinet.addNewCat();
+//        cabinet.addNewDog();
+//        cabinet.addNewCat();
+//        cabinet.addNewDog();
+//        cabinet.printPetListAscendingByID();
+//        cabinet.printPetListAscendingByName();
+//    }
 }
